4c9e9a0c2e01b8c1bd8d45903c38f6ef67d3933f
HuiMuTan <xingdian119@hotmail.com>
2019-09-10 15:32:06 +0800

    删除

--- S03/01.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream> //头文件iostream包含了cout 函数 
-int main(int argc, char** argv) { //main为主函数，入口文件
-	std::cout << "Hello world!\n"; //名字空间std里有cout，
-								   //这里可以加入你的代码
-	system("pause");
-}
