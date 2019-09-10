14e6734aa0da4186d4a801c0c8f00a34b7a19aba
HuiMuTan <xingdian119@hotmail.com>
2019-09-10 15:47:07 +0800

    第一课知识

--- S01/main.cpp
+++ S01/main.cpp
@@ -1,5 +1,17 @@
-#include <iostream>
+//#include <iostream>
+//int main(int argc, char** argv) {
+//	std::cout << "Hello world!\n";
+//}
 
-int main(int argc, char** argv) {
-	std::cout << "Hello world!\n";
+//#include <iostream> //头文件iostream包含了cout 函数 
+//int main(int argc, char** argv) { //main为主函数，入口文件
+//	std::cout << "Hello world!\n"; //名字空间std里有cout，
+//	//这里可以加入你的代码
+//}
+
+#include <iostream> //头文件iostream包含了cout 函数 
+int main(int argc, char** argv) { //main为主函数，入口文件
+//	std::cout<<"4+4="; //也可以使用using namespace std;先声明名字空间
+//	std::cout<<4+4;
+	std::cout<<"4+4="<<4+4; //合到一行
 }
