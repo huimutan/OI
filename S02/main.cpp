afb883b12a5e562e0effc199673af9d2ca958c33
HuiMuTan <xingdian119@hotmail.com>
2019-09-11 17:21:54 +0800

    加入vsc

---
+++ S02/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int litchi;
+    litchi=22;
+    cout<<litchi<<endl;
+    litchi=33;
+    cout<<litchi<<endl;
+    litchi=litchi/3;
+    cout<<litchi<<endl;
+    return 0;
+}
