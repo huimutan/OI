e1fda278f6c2d41fd38288dc7dfb634084bc51e9
HuiMuTan <xingdian119@163.com>
2019-09-13 11:31:47 +0800

    整理代码注释

--- S01/main.cpp
+++ S01/main.cpp
@@ -1,18 +1,12 @@
-//#include <iostream>
-//int main(int argc, char** argv) {
-//	std::cout << "Hello world!\n";
-//}
-
-//#include <iostream> //头文件iostream包含了cout 函数 
-//int main(int argc, char** argv) { //main为主函数，入口文件
-//	std::cout << "Hello world!\n"; //名字空间std里有cout，
-//	//这里可以加入你的代码
-//}
-
 #include <iostream> //头文件iostream包含了cout 函数 
 int main(int argc, char** argv) { //main为主函数，入口文件
-//	std::cout<<"4+4="; //也可以使用using namespace std;先声明名字空间
+//第一个程序，自动生成
+std::cout << "Hello world!\n";//名字空间std里有cout，
+//这里可以加入你的代码
+
+//...........................
+    //第二个程序，输出
+//	std::cout<<"4+4="; //也可以使用using namespace std先声明名字空间，名字空间std里有cout，
 //	std::cout<<4+4;
-	std::cout<<"4+4="<<4+4<<"哈"; //合到一行
-	//
+	std::cout<<"4+4="<<4+4; //合到一行
 }
