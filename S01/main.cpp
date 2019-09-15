ed1c13d80fed33bb8ea51e167b5cf3f4207ca965
huiMuTan <xingdian119@hotmail.com>
2019-09-15 15:03:54 +0800

    新纪录

--- S01/main.cpp
+++ S01/main.cpp
@@ -1,12 +1,36 @@
-#include <iostream> //头文件iostream包含了cout 函数 
-int main(int argc, char** argv) { //main为主函数，入口文件
-//第一个程序，自动生成
-std::cout << "Hello world!\n";//名字空间std里有cout，
-//这里可以加入你的代码
-
-//...........................
-    //第二个程序，输出
-//	std::cout<<"4+4="; //也可以使用using namespace std先声明名字空间，名字空间std里有cout，
-//	std::cout<<4+4;
-	std::cout<<"4+4="<<4+4; //合到一行
+#include <iostream>
+using namespace std; 
+int main(int argc, char** argv) {
+	cout << "Hello world!\n";
+	cout << "4+4=";
+	cout << 4+4;
+	
+	//第一个程序，换行
+  cout<<"人要是行"<<endl;
+  cout<<"干一行"<<endl;
+  cout<<"行一行"<<endl;
+  cout<<"一行行"<<endl;
+  cout<<"行行行"<<endl;
+  cout<<"人要是不行"<<endl;
+  cout<<"干一行"<<endl;
+  cout<<"不行一行"<<endl;
+  cout<<"一行不行"<<endl;
+  cout<<"行行不行"<<endl;
+  //做个试验 
+  int x=1,y=2,z=3;
+  z=x;
+  x=y;
+  y=z;
+  cout<<"x="<<x<<"y="<<y<<"z="<<z;
+  
+  //三角形面积
+  float a=23,h=51,s;
+  s=a*h/2;
+  cout<<s; 
+  
+  cin>>a;
+  return 0; 
+	
 }
+ 
+
