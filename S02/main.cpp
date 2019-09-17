451c875307107d24fdb0d20be38659d1ce4e3016
HuiMuTan <xingdian119@163.com>
2019-09-17 21:15:28 +0800

    修改

--- S02/main.cpp
+++ S02/main.cpp
@@ -3,10 +3,10 @@
 using namespace std;
 int main()
 {
-    //第一个程序，换行
+    //第一个程序，换行...........................
     cout << "人要是行干一行行一行一行行行行行人要是不行干一行不行一行一行不行行行不行。";
-    //...........................
-    //第二个程序，变量的改变
+
+    //第二个程序，变量的改变...........................
     int litchi;
     litchi = 22;
     cout << "litch=" << litchi << endl;
@@ -14,14 +14,25 @@ int main()
     cout << "litch=" << litchi << endl;
     litchi = litchi / 3;
     cout << "litch=" << litchi << endl;
-    //...........................
-    //第三个程序,变量类型的转换
+
+    //第三个程序,变量类型的转换...........................
     int money = 23.5;
     cout << "money=" << money << endl;
-    //...........................
-    //第四个程序,三角形的面积
-    int altitude = 2, base = 4, area;
-    area = altitude * base / 2;
-    cout << "area=" << area << endl;
+
+    //第四个程序,三角形的面积 s=a*h...........................
+    // int a = 3, h = 3;
+    // float s = a * h / 2;
+    // cout << "s=" << s << endl;
+
+    //修改后
+    int a = 3, h = 3;
+    float s = a * h / 2.0; //或强制转换float（2）
+    cout << "s=" << s << endl;
+
+    //第五个程序,学习输入语句...........................
+    int one, two;
+    cin >> one >> two;
+    cout << one << " and " << two;
+
     return 0;
 }
---
+++ codevs.cn/1000.cpp
@@ -0,0 +1,18 @@
+/*
+作者:xingdian119@163.com
+题目:p1000 A+B问题
+*/
+
+
+//如何写一份可以提交的代码？以P1000 A+B为例
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b; //定义两个变量名
+    cin >> a >> b; //从标准输入流中输入两个整数
+    cout << a + b << endl; //输出到标准输出流中
+
+}
+// 完成程序以后，点击下方的提交，即可看到测试结果
+//把原题解法的注释去掉就好了
