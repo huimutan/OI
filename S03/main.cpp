6b88eb6a933d31b6236f6f873b412fb834732650
HuiMuTan <xingdian119@163.com>
2019-10-02 11:03:37 +0800

    课程调整

--- S03/main.cpp
+++ S03/main.cpp
@@ -1,46 +1,31 @@
-#pragma clang diagnostic ignored "-Winvalid-source-encoding"//跳过一个错误提示
+#pragma clang diagnostic ignored "-Winvalid-source-encoding" //跳过一个错误提示
 #include <iostream>
 using namespace std;
 int main()
 {
-    // string a = "草莓";
-    // string b = "芒果";
-    // string c = "火龙果";
-    // cout << "a=" << a << "\nb=" << b << "\nc=" << c << endl;
-    // c = a;
-    // cout << "a=" << a << "\nb=" << b << "\nc=" << c << endl;
-    // a = b;
-    // cout << "a=" << a << "\nb=" << b << "\nc=" << c << endl;
-    // b = c;
-    // cout << "a=" << a << "\nb=" << b << "\nc=" << c;
 
-   //第二个程序，变量的改变
-    int litchi = 22;
-    cout << litchi << endl;
-    litchi = 33;
-    cout << litchi << endl;
+    int strawberry;  //定义一个名字为草莓的变量
+    cout << strawberry << endl; //定义变量不赋值，初始值是随机的
+    strawberry = 1;  //为变量赋值
+    strawberry = 11; //修改变量的值，当新数据存到变量中时，变量由原来的值改为新值。
+    //int strawberry; //再次定义草莓的值，可以吗? 内存中不能有两个相同变量名的区域。
+    int peach = 2, pitaya = 3;
+    //同理定义桃子和火龙果，定义同时赋值。
+
     //...........................
-    //第三个程序,变量类型的转换
-    float money = 23.5; //int
-    cout << money << endl;
-    //...........................
-    //第四个程序,三角形的面积S=a*h/2
-    float a = 2, h = 4, s;
+
+    //三角形的面积S=a*h/2
+    double a = 3,
+           h = 5, s;
     s = a * h / 2;
     cout << s << endl;
-    //第五个程序,学习输入语句...........................
-    int one, two;
-    cin >> one >> two;
-    cout << one << " and " << two;
+
     //尝试以下算式
-    //写一个程序，当输入正方形边长，返回它的面积。S=a*a
-    float a2, s2;
+    //写一个程序，当输入正方形边长，返回它的周长和面积。S=4*a，S=a*a
+    double a2, s2;
     cin >> a2;
     s2 = a2 * a2;
     cout << s2;
-    
 
     return 0;
-
-
 }
