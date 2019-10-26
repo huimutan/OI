a7f0d96e12a0d8e463e4b47afb7000207961e283
HuiMuTan <xingdian119@hotmail.com>
2019-10-26 11:48:07 +0800

    hehe

---
+++ noip/05.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main(){
+    //写代码
+    int c;
+    double f;
+    cin >> c;
+    f = 9.0 / 5 * c + 32;
+
+    cout << fixed << setprecision(2) << f << endl;
+    
+
+};
+
+
+
