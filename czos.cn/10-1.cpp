ec2b143d5c6d55e7336e8ed8d8a943fd36c28bea
HuiMuTan <xingdian119@163.com>
2020-01-16 22:06:39 +0800

    第十课

---
+++ czos.cn/10-1.cpp
@@ -0,0 +1,24 @@
+/* 
+
+*/
+
+#include <iostream>
+using namespace std;
+int main()
+{
+  /*   //初始值
+  int i = 1;
+  //循环条件
+  while (i <= 10)
+  {
+    cout << i << endl;
+    //让循环停止
+    i++;
+  } */
+
+  for (int i = 1; i <= 10; i++)
+  {
+    cout << i << endl;
+  }
+  return 0;
+}
