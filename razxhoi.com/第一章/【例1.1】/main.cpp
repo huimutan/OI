4db8fac25435ff9f6722d08479c98b431bac5ec4
HuiMuTan <xingdian119@163.com>
2020-06-29 21:03:51 +0800

    删除了一些razxhoi的资料

--- razxhoi.com/第一章/【例1.1】/main.cpp
+++
@@ -1,8 +0,0 @@
-#include "picture.h" //加入绘图头文件 
-
-int main()
-{
-  Model.Teapot(50,1);
-  Win.Run(10,3);
-  Win.Show();//启动绘图窗口
-}
--- razxhoi.com/第一章/【题1.4】/main.cpp
+++
@@ -1,8 +0,0 @@
-#include "picture.h" //加入绘图头文件 
-
-int main()
-{
-  Model.ColorPyramid(50,50);
-  Win.Run(10,0);
-  Win.Show();//启动绘图窗口
-}
