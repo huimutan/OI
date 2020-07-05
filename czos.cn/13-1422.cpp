6c112dc74bc2bb9e1cae6e265d0190b61b9439d6
huimutan <xingdian119@hotmail.com>
2020-07-05 16:34:26 +0800

    新文件

---
+++ czos.cn/13-1422.cpp
@@ -0,0 +1,20 @@
+/* 
+1065
+*/
+
+#include <iostream>
+using namespace std;
+int main()
+{
+  int i,n,j;
+  cin>>n;
+  for(i=1;i<=n;i++){
+    for(j=1;j<=n;j++){
+      cout<<i;
+    }
+    cout<<endl;
+  }
+
+
+  return 0;
+}
